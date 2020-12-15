9e9f33ea889ceac288eee89c760c3cf2d480cd03
Greg Lopez <42146599+glopez0909@users.noreply.github.com>
2020-12-15 10:43:18 -0500

    Day 15 Part 2 Solution

--- 2020/Day15/Day15.py
+++ 2020/Day15/Day15.py
@@ -1,25 +1,23 @@
 puzzleInput = [17,1,3,16,19,0]
 
 def dayFifteen(puzzleInput):
-  numbersSaid = puzzleInput.copy()[:-1]
   turnSaid = {17: 1, 1: 2, 3: 3, 16: 4, 19: 5}
   lastNumberSaid = puzzleInput[-1]
   turn = len(puzzleInput)+1
   
 
-  while turn <= 2020:
-    if lastNumberSaid in numbersSaid:
+  while turn <= 30000000:
+    if lastNumberSaid in turnSaid:
       currentSaid = (turn - 1) - turnSaid[lastNumberSaid]
       turnSaid[lastNumberSaid] = turn - 1
       lastNumberSaid = currentSaid
     else:
-      numbersSaid.append(lastNumberSaid)
       turnSaid[lastNumberSaid] = turn - 1
       lastNumberSaid = 0
 
     turn += 1
 
-  print("The answer to part one is", lastNumberSaid)
+  print("The answer to part two is", lastNumberSaid)
 
       
 
